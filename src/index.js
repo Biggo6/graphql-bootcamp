92570d12eaf3cd1b73918aeccdaeb6b0e908afe1
Joram Kimata <joram.kimata@ega.go.tz>
2018-11-30 15:13:07 +0300

    eheee

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { GraphQLServer } from 'graphql-yoga';
 import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import Subscription from './resolvers/Subscription';
 import db from './db';
 import prisma from './prisma';
 
 const server = new GraphQLServer({
     resolvers: {
-        Query
+        Query,
+        Mutation,
+        Subscription
     },
     typeDefs: './src/schema.graphql',
     context: {
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -13,6 +13,20 @@ const Query = {
         return prisma.query.users(opArgs, info);
     },
 
+    comments(parent, args, {db, prisma}, info) {
+        const opArgs = {};
+        if(args.query) {
+            opArgs.where = {
+                OR: [{
+                    name_contains: args.query
+                }, {
+                    email_contains: args.query
+                }]
+            }
+        }
+        return prisma.query.comments(opArgs, info);
+    },
+
     posts(parent, args, { prisma}, info) {
         const opArgs = {};
         if(args.query) {
